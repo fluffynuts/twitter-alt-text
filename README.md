a975f4c529316b20747bb09ba8ae7ef2cd5e0a09
Davyd McColl <davydm@gmail.com>
2021-04-12 21:10:02 +0200

    :sparkles: a bit rough around the edges, but already useful, at least for me

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # twitter-alt-text
 
 Web Extension to show alt-text for images inline in Twitter
+
+
+## what does it do now?
+- adds alt-text elements near images which have alt-text
+- adds a sad message near images which do not have alt-text
+- ignores overview images on the profile page (top-right)
+- ignores emoji images (they already have alt-text)
+
+## what might it do in the future?
+- Provide a quick-link / button to reshare an image with no alt-text,
+  opening up the relevant editor in the Twitter UI so that one could add
+  alt-text. For true bonus points, it should OCR the image & pass off to
+  any free AI image description service to get the user started. High and
+  lofty goals indeed.
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+console.log("---- beep ----");
 (async function () {
   "use strict";
-  console.log("-- start main from twitter-alt-text --");
+  const DEBUG = false;
+  log("-- start main from twitter-alt-text --");
 
   function findParentWithLink(node) {
     let current = node.parentElement;
@@ -30,7 +32,7 @@
     }
     const els = Array.from(document.querySelectorAll("main article span"));
     if (!els.length) {
-      console.log("Can't find a  main article span");
+      log("Can't find a  main article span");
       return null;
     }
     const firstWithColor = els.find(el => !!window.getComputedStyle(el).color);
@@ -46,6 +48,135 @@
     };
   }
 
+  const MAX_DEPTH = 30;
+
+  /**
+   * @param {HTMLElement} el
+   * @returns {HTMLElement[]}
+   */
+  async function enumerateTree(el, depth, first) {
+    return new Promise(resolve => {
+      depth = depth || 0;
+      depth++;
+      first = first || el;
+      if (depth > MAX_DEPTH) {
+        throw new Error(`gave up trying to enumerate tree from`, first);
+      }
+      const immediateChildren = Array.from(el.children);
+      resolve(immediateChildren.reduce(
+        (acc, cur) => {
+          acc.push(enumerateTree(cur, depth, first));
+          return acc;
+        }, [el]).flat(
+        Number.MAX_SAFE_INTEGER
+      ));
+    });
+  }
+
+  /**
+   * @param {HTMLElement} haystack
+   * @param {HTMLElement} needle
+   */
+  async function isAncestor(
+    haystack,
+    needle
+  ) {
+    const tree = await enumerateTree(haystack);
+    return tree.indexOf(needle) > -1;
+  }
+
+  /**
+   * @param {HTMLElement} needle
+   * @param {string} selector
+   * @returns {boolean}
+   */
+  async function isContainedInElementMatching(
+    needle,
+    selector
+  ) {
+    const selectorMatches = Array.from(document.querySelectorAll(selector));
+    for (let cur of selectorMatches) {
+      if (await isAncestor(cur, needle)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * @param {HTMLElement} el
+   * @param {string} testId
+   * @returns {boolean}
+   */
+  async function isContainedInElementWithTestId(
+    el,
+    testId
+  ) {
+    return logResult({
+      label: "isContainedInElementWithTestId",
+      args: [el, testId],
+      result: await isContainedInElementMatching(el, `[testid='${ testId }']`)
+    });
+  }
+
+  /**
+   * @typedef {Object} LogResultInfo
+   * @property {string} label
+   * @property {*[]} args
+   * @property {*} result
+   */
+  /**
+   * @param info
+   */
+  function logResult(info) {
+    log(info.label, info.args);
+    return info.result;
+  }
+
+  function log() {
+    if (!DEBUG) {
+      return;
+    }
+    console.log.apply(console, Array.from(arguments));
+  }
+
+  /**
+   * @param {HTMLImageElement} img
+   */
+  async function shouldIgnore(img) {
+    return isNotImage(img) ||
+      // ignore profile pictures
+      isProfileImage(img) ||
+      // ignore emoji images placed into tweets (which are a good thing)
+      isEmbeddedEmoji(img) ||
+      // profile page: overview of recent tweets / replies
+      false; //await isContainedInElementWithTestId(img, "tweetPhoto");
+  }
+
+  /**
+   * @param {HTMLImageElement} img
+   */
+  function isProfileImage(img) {
+    const src = img.src || "";
+    return src.match(/\/profile_images\//);
+  }
+
+  /**
+   * @param {HTMLElement} node
+   */
+  function isNotImage(node) {
+    return node.tagName !== "IMG";
+  }
+
+  /**
+   * @param {HTMLImageElement} img
+   */
+  function isEmbeddedEmoji(img) {
+    const src = img.src || "";
+    return src.match(/\/emoji\//)
+      || src.match(/twemoji-sprite/);
+  }
+
   function makeSadFace() {
     const sad = document.createElement("img");
     sad.src = "https://abs-0.twimg.com/emoji/v2/svg/1f622.svg";
@@ -56,49 +187,40 @@
     return sad;
   }
 
-  const noAlts = new Set([ "", "image" ]);
+  const noAlts = new Set(["", "image"]);
+  let seenImages = 0;
+
   /**
    * @param {HTMLImageElement} img
    */
   function tryAddAltTextFor(node) {
-
-    const src = node.src || "";
-    if (src.match(/\/profile_images\//)) {
-      // ignore profile pictures
-      return;
-    }
-    if (src.match(/\/emoji\//)) {
-      // ignore emoji images placed into tweets (which are a good thing)
-      return;
-    }
     const
       container = findParentWithLink(node),
       altText = (node.getAttribute("alt") || "").trim().toLowerCase(),
       el = document.createElement("div"),
-      hasNoAltText = noAlts.has(altText);
+      hasNoAltText = noAlts.has(altText),
+      generatedText = hasNoAltText
+        ? " Image has no alt-text "
+        : `[ ${ altText } ]`;
     if (hasNoAltText) {
       const span = document.createElement("span");
-      span.innerText = " Image has no alt-text ";
+      span.innerText = generatedText;
       el.appendChild(makeSadFace());
       el.appendChild(span);
       el.appendChild(makeSadFace());
     } else {
-      el.innerText = `[ ${altText} ]`;
+      el.innerText = generatedText;
     }
-    console.log({
-      hasNoAltText,
-      altText,
-      noAlts
-    });
     if (!container) {
       console.error("Can't find a container for alt-text", node, el);
       return;
     }
     if (container.getAttribute("aria-hidden") === "true") {
-      console.log("ignoring image as parent is marked as aria-hidden", node, el);
+      log("ignoring image as parent is marked as aria-hidden", node, el);
       return;
     }
-    console.log("should add alt for", {
+    log("should add alt for", {
+      generatedText,
       node,
       container,
       el
@@ -112,27 +234,42 @@
     }
     el.style.padding = "5px 10px";
     el.style.fontStyle = "italic";
+    seenImages++;
+    el.setAttribute("data-associated-to-image", seenImages);
+    node.setAttribute("data-associated-to-alt", seenImages);
     container.appendChild(el);
-    console.log("added element", el);
+    log("added element", el);
   }
 
   const observer = new MutationObserver((mutationsList, observer) => {
-    for (const mutation of mutationsList) {
-      if (mutation.type === "childList") {
-        mutation.addedNodes.forEach(node => {
-          if (node.tagName !== "IMG") {
-            return;
-          }
-          // TODO: respond to image deletes (eg thumbnails for videos)
-          // TODO: tie alt text elements to their corresponding images so we
-          //       can remove them too
-          try {
-            tryAddAltTextFor(node);
-          } catch (e) {
-            console.error("tryAddAltTextFor fails:", e);
-          }
-        });
+    try {
+      for (const mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          mutation.addedNodes.forEach(async (node) => {
+            if (await shouldIgnore(node)) {
+              return;
+            }
+            try {
+              tryAddAltTextFor(node);
+            } catch (e) {
+              console.error("tryAddAltTextFor fails:", e);
+            }
+          });
+
+          // // remove alt-texts when their associated images are removed
+          // mutation.removedNodes.forEach(node => {
+          //   if (shouldIgnore(node)) {
+          //     return;
+          //   }
+          //   const
+          //     associationId = node.getAttribute("data-associated-to-alt"),
+          //     associated = Array.from(document.querySelectorAll(`[data-associated-to-image='${ associationId }']`));
+          //   associated.forEach(el => el.remove());
+          // });
+        }
       }
+    } catch (e) {
+      console.error("mutation observer error", e);
     }
   });
 
@@ -140,5 +277,5 @@
     subtree: true,
     childList: true
   });
-  console.log("twitter-alt-text loaded");
+  log("twitter-alt-text loaded");
 })();
